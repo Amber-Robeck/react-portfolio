bbc8301017649518bf9a12da4cd39c22c3bf4b3a
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-03-11 12:21:46 -0600

    added skills to main image page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,18 @@
 import "./intro.scss"
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+
+const scaleVariants = {
+    whileInView: {
+        scale: [0, 1],
+        opacity: [0, 1],
+        transition: {
+            duration: 1,
+            ease: 'easeInOut',
+        },
+    },
+};
 
 export default function Intro() {
     return (
@@ -7,7 +20,17 @@ export default function Intro() {
             <div className="left">
                 <div className="imageCont">
                     <img src="assets/avatar.png" alt="" />
-
+                    <motion.div
+                        variants={scaleVariants}
+                        whileInView={scaleVariants.whileInView}
+                        className="app__header-circles"
+                    >
+                        {[images.node, images.react, images.sass].map((circle, index) => (
+                            <div className="circle-cmp app__flex" key={`circle-${index}`}>
+                                <img src={circle} alt="profile_bg" />
+                            </div>
+                        ))}
+                    </motion.div>
                 </div>
             </div>
             <div className="right">
---
+++ src/constants/images.js
@@ -0,0 +1,22 @@
+
+import css from '../assets/css.png';
+import git from '../assets/git.png';
+import graphql from '../assets/graphql.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import react from '../assets/react.png';
+import sass from '../assets/sass.png';
+import avatar from '../assets/avatar.png';
+
+export default {
+    css,
+    git,
+    graphql,
+    html,
+    javascript,
+    node,
+    react,
+    sass,
+    avatar,
+};
